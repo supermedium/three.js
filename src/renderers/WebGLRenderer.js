366c1278faee4fdd77275e5c097a33d0ab7e1c6d
Felix Z <felixtrz96@gmail.com>
2023-10-20 11:51:11 -0700

    WebXR: Implement antialiased multiview using OCULUS_multiview (#15)
    Fix incorrect handling of depth buffer when stencil buffer was enabled
    
    WebXR: check for null render target (#15) (#17)

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -36,6 +36,7 @@ import { WebGLGeometries } from './webgl/WebGLGeometries.js';
 import { WebGLIndexedBufferRenderer } from './webgl/WebGLIndexedBufferRenderer.js';
 import { WebGLInfo } from './webgl/WebGLInfo.js';
 import { WebGLMorphtargets } from './webgl/WebGLMorphtargets.js';
+import { WebGLMultiview } from './webgl/WebGLMultiview.js';
 import { WebGLObjects } from './webgl/WebGLObjects.js';
 import { WebGLOutput } from './webgl/WebGLOutput.js';
 import { WebGLPrograms } from './webgl/WebGLPrograms.js';
@@ -82,6 +83,7 @@ class WebGLRenderer {
 			failIfMajorPerformanceCaveat = false,
 			reversedDepthBuffer = false,
 			outputBufferType = UnsignedByteType,
+			multiviewStereo = false,
 		} = parameters;
 
 		/**
@@ -420,6 +422,7 @@ class WebGLRenderer {
 		let extensions, capabilities, state, info;
 		let properties, textures, environments, attributes, geometries, objects;
 		let programCache, materials, renderLists, renderStates, clipping, shadowMap;
+		let multiview;
 
 		let background, morphtargets, bufferRenderer, indexedBufferRenderer;
 
@@ -461,6 +464,7 @@ class WebGLRenderer {
 			renderLists = new WebGLRenderLists();
 			renderStates = new WebGLRenderStates( extensions );
 			background = new WebGLBackground( _this, environments, state, objects, _alpha, premultipliedAlpha );
+			multiview = new WebGLMultiview( _this, extensions, _gl );
 			shadowMap = new WebGLShadowMap( _this, objects, capabilities );
 			uniformsGroups = new WebGLUniformsGroups( _gl, info, capabilities, state );
 
@@ -558,7 +562,7 @@ class WebGLRenderer {
 
 		// xr
 
-		const xr = new WebXRManager( _this, _gl );
+		const xr = new WebXRManager( _this, _gl, extensions, multiviewStereo );
 
 		/**
 		 * A reference to the XR manager.
@@ -1740,11 +1744,21 @@ class WebGLRenderer {
 
 					if ( _renderBackground ) background.render( scene );
 
-					for ( let i = 0, l = cameras.length; i < l; i ++ ) {
+					if ( xr.enabled && xr.isMultiview ) {
 
-						const camera2 = cameras[ i ];
+						textures.setDeferTextureUploads( true );
 
-						renderScene( currentRenderList, scene, camera2, camera2.viewport );
+						renderScene( currentRenderList, scene, camera, camera.cameras[ 0 ].viewport );
+
+					} else {
+
+						for ( let i = 0, l = cameras.length; i < l; i ++ ) {
+
+							const camera2 = cameras[ i ];
+
+							renderScene( currentRenderList, scene, camera2, camera2.viewport );
+
+						}
 
 					}
 
@@ -2304,6 +2318,7 @@ class WebGLRenderer {
 			materialProperties.vertexAlphas = parameters.vertexAlphas;
 			materialProperties.vertexTangents = parameters.vertexTangents;
 			materialProperties.toneMapping = parameters.toneMapping;
+			materialProperties.numMultiviewViews = parameters.numMultiviewViews;
 
 		}
 
@@ -2359,6 +2374,8 @@ class WebGLRenderer {
 				}
 
 			}
+
+			const numMultiviewViews = _currentRenderTarget && _currentRenderTarget.isWebGLMultiviewRenderTarget ? _currentRenderTarget.numViews : 0;
 
 			const morphAttribute = geometry.morphAttributes.position || geometry.morphAttributes.normal || geometry.morphAttributes.color;
 			const morphTargetsCount = ( morphAttribute !== undefined ) ? morphAttribute.length : 0;
@@ -2491,6 +2508,10 @@ class WebGLRenderer {
 
 					needsProgramChange = true;
 
+				} else if ( materialProperties.numMultiviewViews !== numMultiviewViews ) {
+
+					needsProgramChange = true;
+
 				}
 
 			} else {
@@ -2558,18 +2579,28 @@ class WebGLRenderer {
 
 				// common camera uniforms
 
-				const reversedDepthBuffer = state.buffers.depth.getReversed();
+				if ( program.numMultiviewViews > 0 ) {
 
-				if ( reversedDepthBuffer && camera.reversedDepth !== true ) {
+					multiview.updateCameraProjectionMatricesUniform( camera, p_uniforms );
+					multiview.updateCameraViewMatricesUniform( camera, p_uniforms );
 
-					camera._reversedDepth = true;
-					camera.updateProjectionMatrix();
+				} else {
+
+					const reversedDepthBuffer = state.buffers.depth.getReversed();
+
+					if ( reversedDepthBuffer && camera.reversedDepth !== true ) {
+
+						camera._reversedDepth = true;
+						camera.updateProjectionMatrix();
+
+					}
+
+					p_uniforms.setValue( _gl, 'projectionMatrix', camera.projectionMatrix );
+
+					p_uniforms.setValue( _gl, 'viewMatrix', camera.matrixWorldInverse );
 
 				}
 
-				p_uniforms.setValue( _gl, 'projectionMatrix', camera.projectionMatrix );
-
-				p_uniforms.setValue( _gl, 'viewMatrix', camera.matrixWorldInverse );
 
 				const uCamPos = p_uniforms.map.cameraPosition;
 
@@ -2765,8 +2796,17 @@ class WebGLRenderer {
 
 			// common matrices
 
-			p_uniforms.setValue( _gl, 'modelViewMatrix', object.modelViewMatrix );
-			p_uniforms.setValue( _gl, 'normalMatrix', object.normalMatrix );
+			if ( program.numMultiviewViews > 0 ) {
+
+				multiview.updateObjectMatricesUniforms( object, camera, p_uniforms );
+
+			} else {
+
+				p_uniforms.setValue( _gl, 'modelViewMatrix', object.modelViewMatrix );
+				p_uniforms.setValue( _gl, 'normalMatrix', object.normalMatrix );
+
+			}
+
 			p_uniforms.setValue( _gl, 'modelMatrix', object.matrixWorld );
 
 			// UBOs
@@ -2881,7 +2921,7 @@ class WebGLRenderer {
 			const renderTargetProperties = properties.get( renderTarget );
 
 			renderTargetProperties.__autoAllocateDepthBuffer = renderTarget.resolveDepthBuffer === false;
-			if ( renderTargetProperties.__autoAllocateDepthBuffer === false ) {
+			if ( renderTargetProperties.__autoAllocateDepthBuffer === false && ( ! _currentRenderTarget || ! _currentRenderTarget.isWebGLMultiviewRenderTarget ) ) {
 
 				// The multisample_render_to_texture extension doesn't work properly if there
 				// are midframe flushes and an external depth buffer. Disable use of the extension.
